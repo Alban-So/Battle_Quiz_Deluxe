5b456ad94712e6df8744e5f4cf392d7d1da7811d
Alban-So <88336264+Alban-So@users.noreply.github.com>
2025-01-16 22:49:58 +0100

    Update partieSave.java

--- Quizz_Battle-main/partieSave.java
+++ Quizz_Battle-main/partieSave.java
@@ -1,8 +1,62 @@
+ // Fonction qui convertit un entier int en String
+    String intToString(int nombre){
+        String nombreString = "";
+        while(nombre != 0){
+            nombreString = (char) (nombre%10 + '0') + nombreString;
+            nombre = nombre/10;
+        }
+        return nombreString;
+    }
+
+// Sauvegarde les informations du plateau
+    void sauvegarderPlateau(Carte[][] plateau){
+
+        String plateauSave[][] = new String[7][2];
+        String nomPlateauSave = "fichiersCSV/plateauSaveTest.csv";
+
+        plateauSave[0][0] = "idCarte";
+        plateauSave[0][1] = "PV";
+
+        int compteur = 1;
+
+       for (int i=0; i<2; i++){ // boucle qui enregistre dans le CSV les cartes du plateau avec leur id et leur PV
+            for (int j=0; j<3; j++) {
+                if (plateau[i][j] != null) {
+                    plateauSave[compteur][0] = intToString(plateau[i][j].idCarte);
+                    plateauSave[compteur][1] = intToString(plateau[i][j].PV);
+                } else {
+                    plateauSave[compteur][0] = "";
+                    plateauSave[compteur][1] = "";
+                }
+                compteur++;
+            }
+       }
+
+       saveCSV(plateauSave, nomPlateauSave);
+
+    }
+
     // Sauvegarde les informations des joueurs
-    /*
-    void sauvegarderInfosJoueur(Joueur joueur1, Joueur joueur2, Joueur joueurQuiJoue, int[] deckJoueur1, int[] deckJoueur2, Carte[] mainJoueur1, Carte[] mainJoueur2, int numTour, int nbrCartesDeckJ1, int nbrCartesDeckJ2){
-        /*String partieSave[][] = new String[3][7];
-        String nomPartieSave = "partieSave.csv";
+    void sauvegarderInfosJoueurs(Joueur joueur1, Joueur joueur2){
+
+        String infosJoueursSave[][] = new String[3][2];
+        String nomInfosJoueursSave = "fichiersCSV/infosJoueursSaveTest.csv";
+
+        infosJoueursSave[0][0] = "pseudo";
+        infosJoueursSave[0][1] = "classe";
+        infosJoueursSave[1][0] = joueur1.pseudo;
+        infosJoueursSave[1][1] = joueur1.classe;
+        infosJoueursSave[2][0] = joueur2.pseudo;
+        infosJoueursSave[2][1] = joueur2.classe;
+
+        saveCSV(infosJoueursSave, nomInfosJoueursSave);
+    }
+
+// Fonction qui sauvegarde les informations principales du jeux
+void sauvegarderInfosJoueur(Joueur joueur1, Joueur joueur2, Joueur joueurQuiJoue, int numTour){
+        
+        String partieSave[][] = new String[3][7];
+        String nomPartieSave = "fichiersCSV/partieSaveTest.csv";
 
         partieSave[0][0] = "numJoueur";
         partieSave[0][1] = "pointsJoueur";
@@ -11,35 +65,39 @@
         partieSave[0][4] = "numTour";
         partieSave[0][5] = "joueurActif";
         partieSave[0][6] = "nbrCartesDeck";
+        partieSave[1][2] = "";
+        partieSave[2][2] = "";
+        partieSave[1][3] = "";
+        partieSave[2][3] = "";
 
         for(int i=1; i<3; i++){
-            partieSave[i][0] = (String) i;
-            partieSave[i][4] = (String) numTour;
-            partieSave[i][5] = (String) joueurQuiJoue;
+            partieSave[i][0] = intToString(i);
+            partieSave[i][4] = intToString(numTour);
+            //partieSave[i][5] = intToString(joueurQuiJoue); // A faire
             if (i == 1){
-                partieSave[i][1] = (String) joueur1.points;
-                partieSave[i][6] = (String) nbrCartesDeckJ1;
+                partieSave[i][1] = intToString(joueur1.points);
+                partieSave[i][6] = intToString(joueur1.nbCarteRestanteDeck);
             }
             else{
-                partieSave[i][1] = (String) joueur2.points;
-                partieSave[i][6] = (String) nbrCartesDeckJ2;
+                partieSave[i][1] = intToString(joueur2.points);
+                partieSave[i][6] = intToString(joueur2.nbCarteRestanteDeck);
             }
         }
 
         for (int i=0; i<TAILLE_DECK; i++){
-            partieSave[1][2] += (String) deckJoueur1[i];
-            partieSave[2][2] += (String) deckJoueur2[i];
+            partieSave[1][2] += intToString(joueur1.deck[i]);
+            partieSave[2][2] += intToString(joueur2.deck[i]);
             if (i<TAILLE_DECK-1) {
                 partieSave[1][2] += "/";
                 partieSave[2][2] += "/";
             }
         }
 
-        for(int i=0; i<length(mainJoueur1); i++){
+        for(int i=0; i<length(joueur1.main); i++){
             
-            if (mainJoueur1[i] != null) partieSave[1][3] += (String) mainJoueur1.idCarte[i];
-            if (mainJoueur2[i] != null) partieSave[2][3] += (String) mainJoueur2.idCarte[i];
-            if (i<length(mainJoueur1)-1) {
+            if (joueur1.main[i] != null) partieSave[1][3] += intToString(joueur1.main[i].idCarte);
+            if (joueur2.main[i] != null) partieSave[2][3] += intToString(joueur2.main[i].idCarte);
+            if (i<length(joueur1.main)-1) {
                 partieSave[1][3] += "/";
                 partieSave[2][3] += "/";
             }
@@ -48,21 +106,4 @@
         saveCSV(partieSave, nomPartieSave);
     }
 
-    // Sauvegarde les informations du plateau
-    void sauvegarderPlateau(Carte[][] plateau){
 
-        String plateauSave[][] = new String[2][7];
-        String nomPlateauSave = "partieSave.csv";
-
-        plateauSave[0][0] = "idCarte";
-        plateauSave[0][1] = "PV";
-        
-
-       for (int i=1; i<4; i++){
-            for (int j=0; j<2; j++){
-                plateauSave[i][j] = plateau.idCarte[j][i];   // test
-                plateauSave[i+3][j] = plateau.idCarte[j][i]; // A verifier
-            }
-       }
-
-    }*/
