4df86f990e7e818182b6846ff0f97609d6573ef4
Morrigano <clementroty70@gmail.com>
2025-01-17 17:43:20 +0100

    Add files via upload

---
+++ Quizz_Battle-main/Joueur.java
@@ -0,0 +1,10 @@
+class Joueur{
+    String pseudo;
+    String classe;
+    int [] deck;
+    Carte [] main;
+    int nbCarteRestanteDeck;
+    int indiceLignePlateauDeCarte;
+    int[][] coordonneesAffichageCartes;
+    int points;
+}
